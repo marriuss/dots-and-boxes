cbb1ebfc1b35cb8cf4b50ee8ac3f8aae24614b15
marriuss <marina.gorbachowa@yandex.ru>
2021-07-13 23:05:55 +0400

    Now Player is an abstract class.

---
+++ dots_and_boxes/classes/players/bot.py
@@ -0,0 +1,16 @@
+import random
+from player import Player
+
+
+class Bot(Player):
+    def __init__(self, color, name):
+        Player.__init__(self, color, name)
+        self.type = "BOT"
+
+    def make_move(self, game):
+        current_state = game[-1]
+        if current_state["gameover"]:
+            self.terminal_state = True
+            return None
+        edge = random.sample(current_state["available_edges"], 1)[0][0]
+        return edge
